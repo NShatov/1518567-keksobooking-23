4c5965c3cb0b11043ee420d6427f0833e5742c9f
NShatov <NShatov86@yandex.ru>
2021-06-07 21:31:31 +0300

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -28,51 +28,39 @@ const getRandomFloat = function(min, max, num) {
 
 const COUNT = 10; //кол-во объектов
 
-const AVATARS = [
-  'img/avatars/user01.png',
-  'img/avatars/user02.png',
-  'img/avatars/user03.png',
-  'img/avatars/user04.png',
-  'img/avatars/user05.png',
-  'img/avatars/user06.png',
-  'img/avatars/user07.png',
-  'img/avatars/user08.png',
-];
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const AVATARS = [];
+
+for (let param = 1; param <= 8; param++) {
+  AVATARS[param] = `img/avatars/user0${param}.png`;
+}
 //offer
 const TITLE = 'Добро пожаловать!'; //заголовок предложения
-const ADDRESS = '23,54'; //адрес предложения
-const PRICE = getRandomInteger(1, 1000); //стоимость
-const TYPE = 'flat'; //тип жилья
-const ROOMS = getRandomInteger(1, 100); //кол-во комнат
-const GUESTS = getRandomInteger(1, 10); //кол-во гостей
-const CHECKIN = '12:00'; //время регистрации
-const CHECKOUT = '12:00'; //время выезда
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel']; //тип жилья
+const CHECKIN = ['12:00', '13:00','14:00']; //время регистрации
+const CHECKOUT = ['12:00', '13:00','14:00']; //время выезда
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; //список услуг
 const DESCRIPTION = 'Это лучшее предложение на рынке!'; //описание
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-//location
-const LAT = getRandomFloat(35.65000,35.70000,5); // широта
-const LNG = getRandomFloat(139.70000,139.80000,5); // долгота
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const createData = () => ({
   avatar: getRandomArrayElement(AVATARS),
   title: TITLE,
-  address: ADDRESS,
-  price: PRICE,
-  type: TYPE,
-  rooms: ROOMS,
-  guests: GUESTS,
-  checkin: CHECKIN,
-  checkout: CHECKOUT,
+  address: '23, 45',
+  price: getRandomInteger(1, 1000),
+  type: getRandomArrayElement(TYPE),
+  rooms: getRandomInteger(1, 100),
+  guests: getRandomInteger(1, 10),
+  checkin: getRandomArrayElement(CHECKIN),
+  checkout: getRandomArrayElement(CHECKOUT),
   features: getRandomArrayElement(FEATURES),
   description: DESCRIPTION,
   photos: getRandomArrayElement(PHOTOS),
-  lat: LAT,
-  lng: LNG,
+  lat: getRandomFloat(35.65000,35.70000,5),
+  lng: getRandomFloat(139.70000,139.80000,5),
 });
 
 const similarData = new Array(COUNT).fill(null).map(() => createData());
