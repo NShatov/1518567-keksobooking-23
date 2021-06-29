2a8ccc4a65e55e759f5f6ad3261dec3407b4f331
NShatov <NShatov86@yandex.ru>
2021-06-29 21:46:20 +0300

    Проверка поля Заголовок

--- js/form.js
+++ js/form.js
@@ -30,4 +30,48 @@ const getInactiveForm = (inactive) => {
 
 getInactiveForm(false);
 
+// валидация формы
+
+// начнем с поиска заголовка формы
+const formTitle = form.querySelector('#title');
+const formPrice = form.querySelector('#price'); // найдем поле с ценой
+
+// зададим максимальные и минимальные значения полей формы
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MIN_PRICE = 0;
+const MAX_PRICE = 1000000;
+
+// изменим способ валидации поля - будем проверять при вводе данных
+formTitle.addEventListener('input', () => {
+  const formTitleLength = formTitle.value.length;
+
+  if (formTitleLength < MIN_TITLE_LENGTH) {
+    formTitle.setCustomValidity('Братан! Минимальная длина 30 символов!');
+  } else if (formTitleLength >= MAX_TITLE_LENGTH) {
+    formTitle.setCustomValidity('Братан! 100 символов достаточно!');
+  } else {
+    formTitle.setCustomValidity('');
+  }
+  formTitle.reportValidity();
+});
+
+formPrice.addEventListener('input', () => {
+  const formPriceValue = formPrice.value;
+
+  if (formPriceValue === MIN_PRICE) {
+    formPrice.setCustomValidity('Братан, живи бесплатно!');
+  } else if (formPriceValue < MIN_PRICE) {
+    formPrice.setCustomValidity('Цена не может быть меньше нуля!');
+  } else if (formPriceValue >= MAX_PRICE) {
+    formPrice.setCustomValidity('Стоп, цена! Хватит!');
+  } else {
+    formPrice.setCustomValidity('');
+  }
+  formPrice.reportValidity();
+});
+
+console.log(formTitle.validity);
+
+
 export {getInactiveForm};
