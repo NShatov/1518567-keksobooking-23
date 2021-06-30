db5706de072c48dfdfa2e4a6a7466c05534324f9
NShatov <nshatov86@yandex.ru>
2021-06-30 14:57:00 +0300

    Добавление условий для работы с select

--- js/form.js
+++ js/form.js
@@ -31,47 +31,77 @@ const getInactiveForm = (inactive) => {
 getInactiveForm(false);
 
 // валидация формы
-
-// начнем с поиска заголовка формы
-const formTitle = form.querySelector('#title');
-const formPrice = form.querySelector('#price'); // найдем поле с ценой
+const formTitle = form.querySelector('#title'); // заголовок формы
+const formPrice = form.querySelector('#price'); // поле с ценой
+const formRooms = form.querySelector('#room_number'); // поле с комнатами
+const formCapacity = form.querySelector('#capacity'); // поле с гостями
 
 // зададим максимальные и минимальные значения полей формы
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
-const MIN_PRICE = 0;
-const MAX_PRICE = 1000000;
+// const MIN_TITLE_LENGTH = 30;
+// const MAX_TITLE_LENGTH = 100;
 
 // изменим способ валидации поля - будем проверять при вводе данных
-formTitle.addEventListener('input', () => {
+/*formTitle.addEventListener('input', () => {
   const formTitleLength = formTitle.value.length;
 
   if (formTitleLength < MIN_TITLE_LENGTH) {
     formTitle.setCustomValidity('Братан! Минимальная длина 30 символов!');
-  } else if (formTitleLength >= MAX_TITLE_LENGTH) {
+  } else if (formTitleLength > MAX_TITLE_LENGTH) {
     formTitle.setCustomValidity('Братан! 100 символов достаточно!');
+  } else if (formTitleLength === 0) {
+    formTitle.setCustomValidity('Братан! Напиши пару строк от души!');
   } else {
     formTitle.setCustomValidity('');
   }
   formTitle.reportValidity();
+});*/
+
+formTitle.addEventListener('invalid', () => {
+  if (formTitle.validity.valueMissing) {
+    formTitle.setCustomValidity('Братан! Напиши пару строк от души!');
+  } else if (formTitle.validity.tooShort) {
+    formTitle.setCustomValidity('Братан! Минимальная длина 30 символов!');
+  } else if (formTitle.validity.tooLong) {
+    formTitle.setCustomValidity('Братан! 100 символов достаточно!');
+  } else {
+    formTitle.setCustomValidity('');
+  }
 });
 
-formPrice.addEventListener('input', () => {
-  const formPriceValue = formPrice.value;
 
-  if (formPriceValue === MIN_PRICE) {
-    formPrice.setCustomValidity('Братан, живи бесплатно!');
-  } else if (formPriceValue < MIN_PRICE) {
-    formPrice.setCustomValidity('Цена не может быть меньше нуля!');
-  } else if (formPriceValue >= MAX_PRICE) {
-    formPrice.setCustomValidity('Стоп, цена! Хватит!');
+// будем проверять поле с ценой после нажатия кнопки "Опубликовать"
+formPrice.addEventListener('invalid', () => {
+  if (formPrice.validity.valueMissing) {
+    formPrice.setCustomValidity('Братан! Нужно ввести значение в поле');
+  } else if (formPrice.validity.rangeUnderflow) {
+    formPrice.setCustomValidity(`Братан! Цена должна быть выше ${formPrice.min}`);
+  } else if (formPrice.validity.rangeOverflow) {
+    formPrice.setCustomValidity(`Братан! Цена не должна быть выше ${formPrice.max}`);
   } else {
     formPrice.setCustomValidity('');
   }
-  formPrice.reportValidity();
 });
 
-console.log(formTitle.validity);
+console.log(formRooms);
+console.log(formRooms.options[0].text);
+console.log(formCapacity.options[2].text);
 
+const getValueForm = () => {
+  const selectedValueRoom = formRooms.options.selectedIndex;
+  const selectedValueCapacity = formCapacity.options.selectedIndex;
+
+  if (formRooms.options[0].selected = true) {
+    formCapacity.options[0].hidden = true;
+    formCapacity.options[1].hidden = true;
+    formCapacity.options[3].hidden = true;
+    formCapacity.options[2].selected = true;
+  } else if (formRooms.options[1].selected = true) {
+    formCapacity.options[0].hidden = true;
+    formCapacity.options[3].hidden = true;
+    formCapacity.options[2].selected = true;
+  }
+};
+
+getValueForm();
 
 export {getInactiveForm};
