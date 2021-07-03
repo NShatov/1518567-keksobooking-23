3d147b6f23df754bbcb178a47e14447d2bcca57f
NShatov <NShatov86@yandex.ru>
2021-07-03 16:53:54 +0300

    Добавление проверок для поля с заголовком и ценой

--- js/form.js
+++ js/form.js
@@ -33,37 +33,39 @@ getInactiveForm(false);
 // валидация формы
 const formTitle = form.querySelector('#title'); // заголовок формы
 const formPrice = form.querySelector('#price'); // поле с ценой
-// const formRooms = form.querySelector('#room_number'); // поле с комнатами
-// const formCapacity = form.querySelector('#capacity'); // поле с гостями
+const formRooms = form.querySelector('#room_number'); // поле с комнатами
+const formCapacity = form.querySelector('#capacity'); // поле с гостями
 
 // зададим максимальные и минимальные значения полей формы
-// const MIN_TITLE_LENGTH = 30;
-// const MAX_TITLE_LENGTH = 100;
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MIN_PRICE = 0;
+const MAX_PRICE = 1000000;
 
 // изменим способ валидации поля - будем проверять при вводе данных
-/*formTitle.addEventListener('input', () => {
+formTitle.addEventListener('input', () => {
   const formTitleLength = formTitle.value.length;
 
   if (formTitleLength < MIN_TITLE_LENGTH) {
-    formTitle.setCustomValidity('Братан! Минимальная длина 30 символов!');
+    formTitle.setCustomValidity('Пользователь! Минимальная длина 30 символов!');
   } else if (formTitleLength > MAX_TITLE_LENGTH) {
-    formTitle.setCustomValidity('Братан! 100 символов достаточно!');
+    formTitle.setCustomValidity('Пользователь! 100 символов достаточно!');
   } else if (formTitleLength === 0) {
-    formTitle.setCustomValidity('Братан! Напиши пару строк от души!');
+    formTitle.setCustomValidity('Пользователь! Напиши пару строк от души!');
   } else {
     formTitle.setCustomValidity('');
   }
   formTitle.reportValidity();
-});*/
+});
 
-// проверка поля с заголовком
+// проверка поля при нажатии кнопки "Опубликовать" с заголовком
 formTitle.addEventListener('invalid', () => {
   if (formTitle.validity.valueMissing) {
-    formTitle.setCustomValidity('Братан! Напиши пару строк от души!');
+    formTitle.setCustomValidity('Пользователь! Напиши пару строк от души!');
   } else if (formTitle.validity.tooShort) {
-    formTitle.setCustomValidity('Братан! Минимальная длина 30 символов!');
+    formTitle.setCustomValidity('Пользователь! Минимальная длина 30 символов!');
   } else if (formTitle.validity.tooLong) {
-    formTitle.setCustomValidity('Братан! 100 символов достаточно!');
+    formTitle.setCustomValidity('Пользователь! 100 символов достаточно!');
   } else {
     formTitle.setCustomValidity('');
   }
@@ -72,14 +74,28 @@ formTitle.addEventListener('invalid', () => {
 // будем проверять поле с ценой после нажатия кнопки "Опубликовать"
 formPrice.addEventListener('invalid', () => {
   if (formPrice.validity.valueMissing) {
-    formPrice.setCustomValidity('Братан! Нужно ввести значение в поле');
+    formPrice.setCustomValidity('Пользователь! Нужно ввести значение в поле');
   } else if (formPrice.validity.rangeUnderflow) {
-    formPrice.setCustomValidity(`Братан! Цена должна быть выше ${formPrice.min}`);
+    formPrice.setCustomValidity(`Пользователь! Цена должна быть выше ${MIN_PRICE}`);
   } else if (formPrice.validity.rangeOverflow) {
-    formPrice.setCustomValidity(`Братан! Цена не должна быть выше ${formPrice.max}`);
+    formPrice.setCustomValidity(`Пользователь! Цена не должна быть выше ${MAX_PRICE}`);
   } else {
     formPrice.setCustomValidity('');
   }
+});
+
+//проверка поля с ценой при вводе данных
+formPrice.addEventListener('input', () => {
+  if (!formPrice.value) {
+    formPrice.setCustomValidity('Пользователь! Нужно ввести значение в поле');
+  } else if (formPrice.value < MIN_PRICE) {
+    formPrice.setCustomValidity(`Пользователь! Цена должна быть выше ${MIN_PRICE}`);
+  } else if (formPrice.value > MAX_PRICE) {
+    formPrice.setCustomValidity(`Пользователь! Цена не должна быть выше ${MAX_PRICE}`);
+  } else {
+    formPrice.setCustomValidity('');
+  }
+  formPrice.reportValidity();
 });
 
 /*const selectedValueRoom = formRooms.options.selectedIndex; // поилучение индекса, выбранного значения
@@ -118,7 +134,7 @@ formCapacity.options[3].hidden = true;*/
 
 // напишем функцию сопастовления данных из одного селекта в другой
 
-/*const getMatchingSelect = function (select1, select2) {
+const getMatchingSelect = function (select1, select2) {
   const matchSelect = {
     1: ['для 1 гостя'],
     2: ['для 1 гостя', 'для 2 гостей'],
@@ -141,11 +157,11 @@ formCapacity.options[3].hidden = true;*/
     }
   }
 
-};*/
+};
 
-/*formRooms.addEventListener('onchange', getMatchingSelect(formRooms, formCapacity));*/
+formRooms.addEventListener('onchange', getMatchingSelect(formRooms, formCapacity));
 
-const getById = (id) => form.querySelector(id);
+/*const getById = (id) => form.querySelector(id);
 
 const disableItems = (element, from, to) => {
   for (let index = from; index < to; index++) {
@@ -168,4 +184,4 @@ const changeDynamicly = () => {
 
 getById('#room_number').addEventListener('change', changeDynamicly);
 
-export {getInactiveForm};
+export {getInactiveForm};*/
